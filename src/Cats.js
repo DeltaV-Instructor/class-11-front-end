c50a16e0d55158667dafc49cc87c2eea96883df5
tektechnologies <info@tektechnologies.com>
2022-08-10 15:42:49 -0500

    add update form

--- src/Cats.js
+++ src/Cats.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import {Button, Container, ListGroup} from 'react-bootstrap';
-
+import UpdateCatForm from './UpdateCatForm.js';
 
  class Cats extends Component {
+  
   render() {
-    console.log('Cat props form App.js',this.props.cats);
+    console.log('Cat props from App.js',this.props.cats);
 
     let cats = this.props.cats.map(cat => (
-        <Cat cat={cat} key={cat._id} deleteCats={this.props.deleteCats} />
+        <Cat 
+          cat={cat}
+          key={cat._id}
+          deleteCats={this.props.deleteCats}  
+          updateCats={this.props.updateCats}
+        />
       // <ListGroup.Item key={cat._id}>{cat.name} is {cat.color}</ListGroup.Item>
 
     ));
@@ -27,14 +33,39 @@ export default Cats;
 
 class Cat extends Component{
   
+  constructor(props){
+    super(props);
+    this.state = {
+      showUpdateForm: false,
+    };
+  }
+
   render(){
     console.log('what can we use in here to delete !!!!!',this.props.cat);
     return(
       <>
       <ListGroup.Item key={this.props.cat._id}>
       {this.props.cat.name} is {this.props.cat.color} 
+
+      <Button 
+        variant="info"
+        onClick={() => this.setState({showUpdateForm: true})}
+        >Update</Button>
+
+
       <Button variant="success" onClick={() => this.props.deleteCats(this.props.cat._id)}>Delete</Button>
       </ListGroup.Item>
+
+      {
+        this.state.showUpdateForm &&
+        <UpdateCatForm 
+        cat={this.props.cat}  
+        updateCats={this.props.updateCats}
+        />
+      }
+
+
+
       </>
     )
   }
