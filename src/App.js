0fbaaed20d5cc4d7a48606d2dfe1b74fad0ddca6
tektechnologies <info@tektechnologies.com>
2023-11-02 13:59:29 -0500

    add old demo

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends React.Component {
   }
 
   getCats = async () => {
-  
+    console.log('did we get here???');
     try {
       let results = await axios.get(`${SERVER}/cats`);
-
+        console.log('results from api', results);
       this.setState({
         cats: results.data
       })
@@ -24,12 +24,16 @@ class App extends React.Component {
       console.log('we have an error: ', error.response.data)
     }
   }
+ //net effect is that when the site loads (I should say this specific componenet loads), the data will be displayed the getCats will be invoked when component mounts after all its tasks. 
+  componentDidMount(){
+    this.getCats();
+  }
 
 
   render() {
-   
+     console.log(this.state.cats);
     let cats = this.state.cats.map(cat => (
-      <p>{cat.name} is {cat.color}</p>
+      <p key={cat._id}>{cat.name} is {cat.color}</p>
     ))
     return (
       <>
